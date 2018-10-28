441b5379400b987a4e980defbe988e78a15aefb8
Shane Whelan <shanewhelan89@gmail.com>
2018-10-28 18:05:05 +0000

    first push

---
+++ django_objects_import.py
@@ -0,0 +1,36 @@
+# How queries work in django.
+# https://www.youtube.com/watch?v=WimXjp0ryOo
+import sys
+import os
+import django
+
+# path of django project.
+sys.path.append('/Users/wheeldogg/Dropbox/Documents/workspace/projects/web-development/django_unleashed/DjangoUnleashed-1.8')
+
+## settings.
+## deprecated
+# from django.core.management import setup_environ
+# setup_environ(settings)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "suorganizer.settings")
+from django.conf import settings
+# django.setup() # don't need to run this.
+
+###################
+# Manager objects.
+###################
+# organizer is name of an app and import the models.
+from organizer import models
+# <module 'django.db.models' from '//anaconda/envs/py3/lib/python3.5/site-packages/django/db/models/__init__.py'>
+
+# startup, newslink and tags are models in the organizer app.
+from organizer.models import Tag
+# <class 'organizer.models.Tag'>
+
+# >>> Tag.objects
+# <django.db.models.manager.Manager object at 0x110d70c18>
+
+# 1st query. Returns a queryset, which behaves like a list effectively.
+tag_objects_sample = Tag.objects.all()
+print(tag_objects_sample)
+# [<Tag: Django>, <Tag: Education>, <Tag: Mobile>, <Tag: Video Games>, <Tag: Web>]
+
---
+++ reticulate.r
@@ -0,0 +1,49 @@
+############################################################  
+# Aim here is to start integrating with python objects.
+# if we use reticulate we can access python objects in R.
+# https://www.r-bloggers.com/r-and-python-how-to-integrate-the-best-of-both-into-your-data-science-workflow/
+library(reticulate)
+conda_list()
+
+# Note if you have issues with loading the correct environment you need to restart R.
+# more info
+# reticulate::py_config()
+# select conda env.
+use_condaenv("//anaconda/envs/py3/bin/python")
+# use_condaenv("//anaconda/envs/r-reticulate/bin/python")
+# or 
+path_to_python <- "//anaconda/envs/py3/bin/python"
+use_python(path_to_python)
+
+# packageVersion("reticulate")
+repl_python()
+
+# Sourcing Python scripts — 
+# The source_python() function enables you to source 
+# a Python script the same way you would source() an R script 
+# (Python functions and objects defined within the script become directly available to the R session).
+
+# source activate r-reticulate
+# example.
+# import your django models into R.
+# django_app                 <- "/Users/wheeldogg/Dropbox/Documents/workspace/projects/web-development/django_unleashed/DjangoUnleashed-1.8"
+python_read_django_objects <- "/Users/wheeldogg/Dropbox/Documents/workspace/projects/web-development/access_objects_in_r/python/django_objects_import.py"
+
+# source the python file.
+source_python(python_read_django_objects)
+# class(source_python(python_read_django_objects))
+
+############################################################  
+# run python directly.
+# repl_python()
+# import pandas
+# exit
+# Enter exit within the Python REPL to return to the R prompt.
+# Note that Python code can also access objects from within the R session using the r object (e.g. r.flights).
+# See the repl_python() documentation for additional details on using the embedded Python REPL.
+############################################################
+############################################################  
+# To run this in the command line.
+# Rscript -e 'Sys.which("python"); library(reticulate); sys <- reticulate::import("sys"); sys$executable;   packageVersion("reticulate"); reticulate::py_config()'
+# python 
+############################################################  
